f87fbf3985e1d62535f50544b4218c07dec449d9
pragti-124 <pragti2056.be22@chitkara.edu.in>
2024-07-29 15:27:02 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,87 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+// Background music
+const bgMusic = document.querySelector("#bg-music");
+
+document.addEventListener("DOMContentLoaded", () => {
+  bgMusic.play().catch(error => {
+    console.log("Background music autoplay was prevented. It will play on the first user interaction.");
+  });
+});
+
+// Sound effects for choices
+const soundEffects = {
+  rock: new Audio('rps.wav'),
+  paper: new Audio('rps.wav'),
+  scissors: new Audio('rps.wav')
+};
+
+const genCompChoice = () => {
+  const options = ["rock", "paper", "scissors"];
+  const randIdx = Math.floor(Math.random() * 3);
+  return options[randIdx];
+};
+
+const drawGame = () => {
+  msg.innerText = "Game was Draw. Play again.";
+  msg.style.backgroundColor = "#081b31";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if (userWin) {
+    userScore++;
+    userScorePara.innerText = userScore;
+    msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
+    msg.style.backgroundColor = "green";
+  } else {
+    compScore++;
+    compScorePara.innerText = compScore;
+    msg.innerText = `You lost. ${compChoice} beats your ${userChoice}`;
+    msg.style.backgroundColor = "red";
+  }
+};
+
+const playGame = (userChoice) => {
+  // Generate computer choice
+  const compChoice = genCompChoice();
+
+  // Play the corresponding sound effect
+  soundEffects[userChoice].play();
+
+  if (userChoice === compChoice) {
+    // Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      // scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      // rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      // rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
+
+    // Ensure background music starts on first user interaction
+    bgMusic.play().catch(error => {
+      console.log("Background music autoplay was prevented. It will play on the first user interaction.");
+    });
+  });
+});
